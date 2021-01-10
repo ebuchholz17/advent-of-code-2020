66d64167d1333a716a75541e6112f33112e09156
Erik Buchholz <erik.buchholz@arkadium.com>
2021-01-10 10:28:40 -0500

    day 16 part 1

---
+++ 16/main.cpp
@@ -0,0 +1,175 @@
+#include "advent.h"
+
+struct field_range {
+    int min;
+    int max;
+};
+
+#define FIELDS_PER_TICKET 20
+
+struct ticket_field {
+    char name[20];
+    field_range ranges[2];
+    bool validIndices[FIELDS_PER_TICKET];
+};
+
+#define LIST_TYPE ticket_field
+#include "list.h"
+
+
+struct plane_ticket {
+    int fields[FIELDS_PER_TICKET];
+};
+
+#define LIST_TYPE plane_ticket
+#include "list.h"
+
+#define LIST_TYPE int
+#include "list.h"
+
+void readTicketInput (char *line, plane_ticket *ticket) {
+    //void *inputs[] = {
+    //    &ticket->fields[0], 
+    //    &ticket->fields[1],
+    //    &ticket->fields[2]
+    //};
+    //parseInput(line, "%d,%d,%d", inputs);
+
+    void *inputs[] = {
+        &ticket->fields[0], 
+        &ticket->fields[1],
+        &ticket->fields[2],
+        &ticket->fields[3],
+        &ticket->fields[4],
+        &ticket->fields[5],
+        &ticket->fields[6],
+        &ticket->fields[7],
+        &ticket->fields[8],
+        &ticket->fields[9],
+        &ticket->fields[10],
+        &ticket->fields[11],
+        &ticket->fields[12],
+        &ticket->fields[13],
+        &ticket->fields[14],
+        &ticket->fields[15],
+        &ticket->fields[16],
+        &ticket->fields[17],
+        &ticket->fields[18],
+        &ticket->fields[19]
+    };
+    parseInput(line, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", inputs);
+}
+
+int main (int argc, char **argv) {
+    memory_arena memory = {};
+    memory.capacity = 1 * 1024 * 1024;
+    memory.base = malloc(memory.capacity);
+
+    const int lineLength = 100;
+    char line[lineLength];
+
+    ticket_field_list ticketFields = ticket_fieldListInit(&memory, 20);
+    plane_ticket myTicket = {};
+    plane_ticket_list nearbyTickets = plane_ticketListInit(&memory, 300);
+
+    fgets(line, lineLength, stdin);
+    char *lineCursor = line;
+    while (*lineCursor != '\n') {
+        ticket_field ticketField = {};
+        lineCursor = readUntilCharacter(lineCursor, ticketField.name, ':');
+        ++lineCursor;
+
+        {
+            void *inputs[] = {&ticketField.ranges[0].min, 
+                              &ticketField.ranges[0].max,
+                              &ticketField.ranges[1].min,
+                              &ticketField.ranges[1].max};
+            parseInput(lineCursor, " %d-%d or %d-%d", inputs);
+        }
+        listPush(&ticketFields, ticketField);
+
+        fgets(line, lineLength, stdin);
+        lineCursor = line;
+    }
+
+    fgets(line, lineLength, stdin);
+    fgets(line, lineLength, stdin);
+
+    readTicketInput(line, &myTicket);
+
+    fgets(line, lineLength, stdin);
+    fgets(line, lineLength, stdin);
+
+    while (fgets(line, lineLength, stdin)) {
+        plane_ticket nearbyTicket = {};
+        readTicketInput(line, &nearbyTicket);
+        listPush(&nearbyTickets, nearbyTicket);
+    }
+
+    int_list invalidFields = intListInit(&memory, FIELDS_PER_TICKET * nearbyTickets.numValues);
+    for (int i = nearbyTickets.numValues - 1; i >= 0; --i) {
+        plane_ticket *ticket = &nearbyTickets.values[i];
+        bool anyFieldWasInvalid = false;
+
+        for (int j = 0; j < FIELDS_PER_TICKET; ++j) {
+            int field = ticket->fields[j];
+            bool fieldIsValid = false;
+
+            for (int k = 0; k < ticketFields.numValues; ++k) {
+                ticket_field ticketField = ticketFields.values[k];
+                if ((field >= ticketField.ranges[0].min && field <= ticketField.ranges[0].max) ||
+                    (field >= ticketField.ranges[1].min && field <= ticketField.ranges[1].max))
+                {
+                    fieldIsValid = true;
+                    break;
+                }
+            }
+
+            if (!fieldIsValid) {
+                listPush(&invalidFields, field);
+                anyFieldWasInvalid = true;
+            }
+        }
+
+        if (anyFieldWasInvalid) {
+            listSplice(&nearbyTickets, i);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < invalidFields.numValues; ++i) {
+        sum += invalidFields.values[i];
+    }
+
+    printf("%d\n", sum);
+
+    // part 2
+    for (int fieldIndex = 0; fieldIndex < ticketFields.numValues; ++fieldIndex) {
+        ticket_field *ticketField = &ticketFields.values[fieldIndex];
+        for (int ticketFieldIndex = 0; ticketFieldIndex < FIELDS_PER_TICKET; ++ticketFieldIndex) {
+            ticketField->validIndices[ticketFieldIndex] = true;
+            for (int ticketIndex = 0; ticketIndex < nearbyTickets.numValues; ++ticketIndex) {
+                int fieldValue = nearbyTickets.values[ticketIndex].fields[ticketFieldIndex];
+                if ((fieldValue >= ticketField->ranges[0].min && fieldValue <= ticketField->ranges[0].max) ||
+                    (fieldValue >= ticketField->ranges[1].min && fieldValue <= ticketField->ranges[1].max))
+                {
+
+                }
+                else {
+                    ticketField->validIndices[ticketFieldIndex] = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    memory_arena tempMemory = {};
+    tempMemory.capacity = 512 * 1024 * 1024;
+    tempMemory.base = malloc(tempMemory.capacity);
+
+
+
+    tempMemory.size = 0;
+
+    return 0;
+}
---
+++ 16/win_console.cpp
@@ -0,0 +1,308 @@
+#include <windows.h>
+#include <fileapi.h>
+
+#include <timeapi.h>
+
+struct win_console {
+    HANDLE inputHandle;
+    HANDLE outputHandle;
+    HANDLE standardOutputHandle;
+
+    SMALL_RECT windowDimensions;
+    COORD windowBufferSize;
+    COORD windowWriteCoord;
+    CHAR_INFO *windowBuffer;
+
+    long long perfCountFrequency;
+    LARGE_INTEGER lastCounter;
+    float targetMSPerFrame;
+};
+
+struct win_console_key {
+    bool down;
+    bool justPressed;
+};
+
+struct win_console_input {
+    win_console_key upKey;
+    win_console_key downKey;
+    win_console_key leftKey;
+    win_console_key rightKey;
+};
+
+struct win_console_window {
+    int width;
+    int height;
+    char *buffer;
+};
+
+void initWinConsole (win_console *winConsole, float targetMSPerFrame) {
+    winConsole->inputHandle = 
+        CreateFileA("CONIN$",
+                    GENERIC_READ | GENERIC_WRITE,
+                    FILE_SHARE_READ,
+                    0,
+                    OPEN_EXISTING,
+                    FILE_ATTRIBUTE_NORMAL,
+                    0);
+    winConsole->standardOutputHandle = 
+        CreateFileA("CONOUT$",
+                    GENERIC_READ | GENERIC_WRITE,
+                    FILE_SHARE_WRITE,
+                    0,
+                    OPEN_EXISTING,
+                    FILE_ATTRIBUTE_NORMAL,
+                    0);
+    winConsole->outputHandle = 
+        CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE,
+                                  FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                  0,
+                                  CONSOLE_TEXTMODE_BUFFER,
+                                  0);
+    CONSOLE_CURSOR_INFO cursorInfo = {};
+    cursorInfo.dwSize = 1;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(winConsole->outputHandle, &cursorInfo);
+
+    LARGE_INTEGER perfCountFrequencyResult;
+    QueryPerformanceFrequency(&perfCountFrequencyResult);
+    winConsole->perfCountFrequency = perfCountFrequencyResult.QuadPart;
+    winConsole->targetMSPerFrame = targetMSPerFrame;
+}
+
+void setWinConsoleOutputActive (win_console *winConsole) {
+    SetConsoleActiveScreenBuffer(winConsole->outputHandle);
+}
+
+void setWinConsoleStandardOutputActive (win_console *winConsole) {
+    SetConsoleActiveScreenBuffer(winConsole->standardOutputHandle);
+}
+
+int initWinConsoleBuffer (win_console *winConsole, int width, int height) {
+    winConsole->windowDimensions = {};
+    winConsole->windowDimensions.Left = 0;
+    winConsole->windowDimensions.Top = 0;
+    winConsole->windowDimensions.Bottom = (SHORT)height - 1;
+    winConsole->windowDimensions.Right = (SHORT)width - 1;
+
+    winConsole->windowBufferSize = {};
+    winConsole->windowBufferSize.X = (SHORT)width;
+    winConsole->windowBufferSize.Y = (SHORT)height;
+
+    winConsole->windowWriteCoord = {};
+    winConsole->windowWriteCoord.X = 0;
+    winConsole->windowWriteCoord.Y = 0;
+    
+    CONSOLE_SCREEN_BUFFER_INFOEX screenBufferInfo;
+
+    ZeroMemory(&screenBufferInfo, sizeof(CONSOLE_SCREEN_BUFFER_INFOEX));
+    screenBufferInfo.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+    
+    GetConsoleScreenBufferInfoEx(winConsole->outputHandle, &screenBufferInfo);
+    
+    screenBufferInfo.dwCursorPosition = winConsole->windowWriteCoord;
+    screenBufferInfo.dwSize = winConsole->windowBufferSize;
+    screenBufferInfo.srWindow.Left = 0;
+    screenBufferInfo.srWindow.Top = 0;
+    screenBufferInfo.srWindow.Right = (SHORT)width;
+    screenBufferInfo.srWindow.Bottom = (SHORT)height;
+
+    SetConsoleScreenBufferInfoEx(winConsole->outputHandle, &screenBufferInfo);
+
+    return sizeof(CHAR_INFO) * width * height;
+} 
+
+void copyLStr (WCHAR *buffer, WCHAR *string) {
+    int index = 0;
+    while (string[index] != 0 && index < LF_FACESIZE) {
+        buffer[index] = string[index];
+        ++index;
+    }
+}
+
+void winConsoleSetFont (win_console *winConsole, int sizeX, int sizeY, WCHAR *fontName, int fontWeight) {
+    CONSOLE_FONT_INFOEX fontInfo = {};
+    fontInfo.cbSize = sizeof(CONSOLE_FONT_INFOEX);
+
+    GetCurrentConsoleFontEx(winConsole->outputHandle, false, &fontInfo);
+
+    copyLStr(fontInfo.FaceName, fontName);
+    fontInfo.dwFontSize.X = (SHORT)sizeX;
+    fontInfo.dwFontSize.Y = (SHORT)sizeY;
+
+    fontInfo.FontFamily = FF_DONTCARE;
+    fontInfo.FontWeight = fontWeight;
+
+    SetCurrentConsoleFontEx(winConsole->outputHandle, false, &fontInfo);
+
+    CONSOLE_SCREEN_BUFFER_INFOEX screenBufferInfo = {};
+    screenBufferInfo.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+    
+    // resize window
+    // TODO(ebuchholz): move to separate function?
+    GetConsoleScreenBufferInfoEx(winConsole->outputHandle, &screenBufferInfo);
+
+    screenBufferInfo.srWindow.Right = (SHORT)(winConsole->windowBufferSize.X);
+    screenBufferInfo.srWindow.Bottom = (SHORT)(winConsole->windowBufferSize.Y);
+
+    SetConsoleScreenBufferInfoEx(winConsole->outputHandle, &screenBufferInfo);
+
+    //SetConsoleScreenBufferSize(winConsole->outputHandle, winConsole->windowBufferSize);
+}
+
+void beginPreciseWindowsTiming () {
+    timeBeginPeriod(1);
+}
+
+void endPreciseWindowsTiming () {
+    timeEndPeriod(1);
+}
+
+void setWindowBufferMemory (win_console *winConsole, void *memory) {
+    winConsole->windowBuffer = (CHAR_INFO *)memory;
+
+    // blank out the buffer to start
+    for (int i = 0; i < winConsole->windowBufferSize.Y; ++i) {
+        for (int j = 0; j < winConsole->windowBufferSize.X; ++j) {
+            CHAR_INFO charInfo = {};
+            charInfo.Char.AsciiChar = ' ';
+            charInfo.Attributes = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY;
+            winConsole->windowBuffer[i * winConsole->windowBufferSize.X + j] = charInfo;
+        }
+    }
+}
+
+void startWinConsoleFrame (win_console *winConsole) {
+    QueryPerformanceCounter(&winConsole->lastCounter);
+}
+
+// TODO(ebuchholz): more precise timer: this tends to sleep too long
+void waitForEndOfWinConsoleFrame (win_console *winConsole) {
+    LARGE_INTEGER workCounter;
+    QueryPerformanceCounter(&workCounter);
+    long long counterElapsed = workCounter.QuadPart - winConsole->lastCounter.QuadPart;
+    float elapsedMS = (1000.0f * (float)counterElapsed) / (float)winConsole->perfCountFrequency;
+
+    if (elapsedMS < winConsole->targetMSPerFrame) {
+        DWORD sleepMS = (DWORD)(winConsole->targetMSPerFrame - elapsedMS);
+        if (sleepMS > 0) {
+            Sleep(sleepMS);
+        }
+    }
+}
+
+void renderWinConsole (win_console *winConsole) {
+    WriteConsoleOutput(winConsole->outputHandle,
+                       winConsole->windowBuffer,       
+                       winConsole->windowBufferSize,   
+                       winConsole->windowWriteCoord,   
+                       &winConsole->windowDimensions); 
+}
+
+void winConsoleWriteText (win_console *winConsole, char *text, int length, int x, int y) {
+    for (int i = 0; i < length; ++i) {
+        char letter = text[i];
+        if (letter == 0) {
+            break;
+        }
+        CHAR_INFO *charInfo = &winConsole->windowBuffer[y * winConsole->windowBufferSize.X + x + i];
+        charInfo->Char.AsciiChar = letter;
+    }
+}
+
+// assumes the characters buffer is the right size
+void writeWinConsoleCharsToRect (win_console *winConsole, char *characters, 
+                       int x, int y, int width, int height) 
+{
+    // TODO(ebuchholz)
+}
+
+void writeWinConsoleCharAtXY (win_console *winConsole, char character, int x, int y) {
+    CHAR_INFO *charInfo = &winConsole->windowBuffer[y * winConsole->windowBufferSize.X + x];
+    charInfo->Char.AsciiChar = character;
+}
+
+void writeWinConsoleCharsToWholeScreen (win_console *winConsole, char *characters) {
+    for (int i = 0; i < winConsole->windowBufferSize.Y; ++i) {
+        for (int j = 0; j < winConsole->windowBufferSize.X; ++j) {
+            CHAR_INFO *charInfo = &winConsole->windowBuffer[i * winConsole->windowBufferSize.X + j];
+            charInfo->Char.AsciiChar = characters[i * winConsole->windowBufferSize.X + j];
+        }
+    }
+}
+
+void releaseWinConsoleHandles (win_console *winConsole) {
+    CloseHandle(winConsole->outputHandle);
+    CloseHandle(winConsole->standardOutputHandle);
+    CloseHandle(winConsole->inputHandle);
+}
+
+void getWinConsoleInput (win_console *winConsole, win_console_input *input) {
+    INPUT_RECORD inputs[100];
+    DWORD numInputsRead;
+    PeekConsoleInput(winConsole->inputHandle, inputs, 100, &numInputsRead);
+
+    for (unsigned int i = 0; i < numInputsRead; ++i) {
+        INPUT_RECORD inputRecord = inputs[i];
+        if (inputRecord.EventType == KEY_EVENT) {
+            KEY_EVENT_RECORD keyEvent = inputRecord.Event.KeyEvent;
+            switch (keyEvent.wVirtualKeyCode) {
+                case VK_UP: {
+                    if (keyEvent.bKeyDown) {
+                        if (!input->upKey.down) {
+                            input->upKey.justPressed = true;
+                        }
+                        input->upKey.down = true;
+                    }
+                    else { 
+                        input->upKey.down = false;
+                    }
+                } break;
+                case VK_DOWN: {
+                    if (keyEvent.bKeyDown) {
+                        if (!input->downKey.down) {
+                            input->downKey.justPressed = true;
+                        }
+                        input->downKey.down = true;
+                    }
+                    else { 
+                        input->downKey.down = false;
+                    }
+                } break;
+                case VK_LEFT: {
+                    if (keyEvent.bKeyDown) {
+                        if (!input->leftKey.down) {
+                            input->leftKey.justPressed = true;
+                        }
+                        input->leftKey.down = true;
+                    }
+                    else { 
+                        input->leftKey.down = false;
+                    }
+                } break;
+                case VK_RIGHT: {
+                    if (keyEvent.bKeyDown) {
+                        if (!input->rightKey.down) {
+                            input->rightKey.justPressed = true;
+                        }
+                        input->rightKey.down = true;
+                    }
+                    else { 
+                        input->rightKey.down = false;
+                    }
+                } break;
+            }
+        }
+    }
+
+    FlushConsoleInputBuffer(winConsole->inputHandle);
+}
+
+void winConsoleResetInput (win_console_input *input) {
+    input->upKey.justPressed = false;
+    input->downKey.justPressed = false;
+    input->leftKey.justPressed = false;
+    input->rightKey.justPressed = false;
+}
+
+
